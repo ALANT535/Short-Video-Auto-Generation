9ced28f6c6bef4925e4e1edcd81f16bd55288441
Alan Thomas <thomasalan535@gmail.com>
2024-05-01 01:49:38 +0530

    excel working again

--- src/excel_operataions.py
+++ src/excel_operataions.py
@@ -2,41 +2,49 @@ import os
 import pandas as pd
 
 parent_directory = "\\".join(os.path.abspath(__file__).split("\\")[:-2])
-excel_path = os.path.join(parent_directory,"Excel Files","counters.xlsx")
+excel_path = os.path.join(parent_directory,"Excel Files","test.xlsx")
 
 def get_counter(subreddit):
     try:
         df = pd.read_excel(excel_path)
+        print(df.columns)
+        
     except:
         print("couldnt open excel file.")
+        
+    print(df["SubReddit Name"].values)
     
     #if the subreddit has already been used before, then consider videos after that counter
-    if (subreddit in df["SubReddit Name"]):
-        return df.iloc[subreddit]["counter"]
+    if (subreddit in df["SubReddit Name"].values):
+        return df.loc[subreddit,"Counter"]
+    
     #else start from the first post
     else:
         return 0
 
+
 def write_counter(subreddit, counter):
     try:
         df = pd.read_excel(excel_path)
-        print(df.columns)
+        
     except:
-        print("couldnt open excel file.")
+        print("ERROR : couldnt open excel file.")
+        return
     if subreddit in df['SubReddit Name'].values:
         # if the subreddit exists, update the counter
         df.loc[df['SubReddit Name'] == subreddit, 'Counter'] = counter
     else:
         # if the subreddit doesn't exist, append a new row
-        df = df._append({'SubReddit Name': subreddit, 'Counter': counter}, ignore_index=True)
+        df_temp = pd.DataFrame({"SubReddit Name" : [subreddit],"Counter" : [counter]})
+        df = pd.concat([df,df_temp])
     
-    # df.rename(columns={'row': 'Sl.No'}, inplace=True)
 
-    # write back to the dataframe with SL.No starting from 1
-    df.to_excel(excel_path, index=True, startrow=1)
+    # write back to the excel file
+    df.to_excel(excel_path,index = False)
     
 # example usage
-# counter = get_counter("Unexpected")
-# print(counter)
+# print(get_counter("Unexpected"))
+# write_counter("Unexpected",45)
 
-write_counter("Unexpected",47)
+print(get_counter("Memes"))
+# print(get_counter("Unexpected"))
--- src/excelwork.py
+++ src/excelwork.py
@@ -1,6 +1,19 @@
-from excel_operataions import get_counter
+# from excel_operataions import get_counter
 import pandas as pd
 
 
-excel_path = r"Excel Files\counters.xlsx"
-df = pd.Dataframe()
+excel_path = r"Excel Files\test.xlsx"
+# df = pd.read_excel(excel_path)
+
+# df = pd.DataFrame({"SubReddit Name":["Unexpected"],"Counter":[4]})
+
+# df = pd.concat([df,df11])
+
+
+# df11 = pd.read_excel(excel_path)
+
+df12 = pd.DataFrame({"SubReddit Name" : ["Memes"],"Counter" : [45]})
+
+# df11 = pd.concat([df11,df12])
+df12.to_excel(excel_path,index = False)
+print(df12.columns)
