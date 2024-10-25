e1f61738718f4b3749c1e0365546f05b909c189b
Alan Thomas <thomasalan535@gmail.com>
2024-10-25 20:42:19 +0530

    changes

--- src/learning_fastapi/main.py
+++ src/learning_fastapi/main.py
@@ -18,7 +18,7 @@ def read_root():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, q: Union[str, None]):
     return {"item_id": item_id, "q": q}
 
 
