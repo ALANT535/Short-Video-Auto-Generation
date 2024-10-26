195829d079b28055ecff02613dc2031f7e5825ae
Alan Thomas <thomasalan535@gmail.com>
2024-10-26 22:40:14 +0530

    finally figured out FASTAPI UploadFile

--- src/learning_fastapi/main.py
+++ src/learning_fastapi/main.py
@@ -1,27 +1,19 @@
-from typing import Union
+from typing import Annotated
 
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
-
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def root_function():
+    return {"This is the Index page, welcome here!"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None]):
-    return {"item_id": item_id, "q": q}
+# This will be the endpoint to upload the files
+@app.post("/uploadfiles/")
+async def uploadfile(file:UploadFile):
+    return {"name":file.filename}
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+# This will then be connected to a public server link
+# This will be fetched using a GET method
