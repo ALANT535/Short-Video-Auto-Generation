4013936e4d6737ccddcc5fc0f8c6fb584674675b
Alan Thomas <thomasalan535@gmail.com>
2024-10-28 12:57:24 +0530

    why didnt go through

--- src/learning_fastapi/main.py
+++ src/learning_fastapi/main.py
@@ -1,12 +1,12 @@
 from typing import Annotated
-
+from fastapi.responses import FileResponse
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from pathlib import Path 
 
 app = FastAPI()
 
-UPLOAD_FILE_PATH = Path("uploaded_files/merged.txt")
-UPLOAD_FILE_PATH.parent.mkdir(exist_ok = True)
+UPLOAD_FILE_PATH = Path("uploaded_files/merged.mp4")
+UPLOAD_FILE_PATH.parent.mkdir(parents=True,exist_ok = True)
 
 @app.get("/")
 async def root_function():
@@ -14,27 +14,30 @@ async def root_function():
 
 # This is the endpoint that will upload the actual video file
 @app.post("/uploadfile/")
-async def upload_file(file:Annotated[UploadFile , File("This file is being uploaded and is of type UploadFile")]):
+async def upload_file(file:UploadFile):
     try:
-        with open(UPLOAD_FILE_PATH , "w") as f:
+        with open(UPLOAD_FILE_PATH , "wb") as f:
             f.write(await file.read())
-    except:
-        return {"filename":file.filename , "Status":400}
+            
+    except Exception as e:  # Catch any file related exceptions
+        raise HTTPException(status_code=400, detail=str(e))
     
     return {"filename":file.filename , "Status":200}
 
 # 400 status code means not successful - ERROR
 # 200 status code means was successful - OKAY    
 
-@app.get("uploaded_files")
-async def get_details():
+
+# This will be fetched using a GET method
+@app.get("/download/")
+async def download_file():
     if not UPLOAD_FILE_PATH.exists():
         raise HTTPException(status_code=404, detail="File not found")
-
-    with open(UPLOAD_FILE_PATH , "r") as f:
-        text_read = f.read()
     
-    return {"content read":text_read}
-
+    return FileResponse(UPLOAD_FILE_PATH)
 # This will then be connected to a public server link
-# This will be fetched using a GET method
+
+# This is used to return the Public Video Link
+@app.get("/video_url/")
+async def get_video_url():
+    return {"video_url": f"http://127.0.0.1:8000/download/"}
