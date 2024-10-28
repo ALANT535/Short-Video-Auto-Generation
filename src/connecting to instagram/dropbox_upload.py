0e924fa141abd5e72c091afd7fd309afc5302101
Alan Thomas <thomasalan535@gmail.com>
2024-10-28 23:58:49 +0530

    dropbox upload

--- src/connecting to instagram/dropbox_upload.py
+++ src/connecting to instagram/dropbox_upload.py
@@ -2,5 +2,12 @@ import dropbox
 
 db = dropbox.Dropbox("dn0bcugrtiw3mju")
 
+file_path = r"C:\Users\LENOVO\Documents\Important_documents\VIT\Projects\REEL_AUTOMATION\output\merged_.mp4"
 
+# The folder where the video will be saved
+dropbox_destination_path = r"genreel_content/merged_.mp4"
+
+
+with open(file_path, 'rb') as f:
+    db.files_upload(f.read(), dropbox_destination_path, mode=dropbox.files.WriteMode("overwrite"))
 
