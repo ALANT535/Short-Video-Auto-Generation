3e05a6cb6ed572e06e3faad3d34427cc357bd9e8
Alan Thomas <thomasalan535@gmail.com>
2024-11-01 23:02:40 +0530

    adding kahns

--- src/connecting to instagram/dropbox_upload.py
+++ src/connecting to instagram/dropbox_upload.py
@@ -3,10 +3,10 @@ from fastapi import HTTPException
 from generate_access_token import *
 
 
-APP_KEY = 'dn0bcugrtiw3mju'
-APP_SECRET = '848vzcex6fymjfb'
+APP_KEY = ''
+APP_SECRET = ''
 
-REFRESH_TOKEN = 'Itu-8RIbsBgAAAAAAAAAAc2xSEVuQec84gJJsOFcAXYbLFAzJQm7MH9RTADFufMS'
+REFRESH_TOKEN = ''
 
 def upload_to_dropbox():
     
@@ -31,11 +31,13 @@ def upload_to_dropbox():
     def ensure_folder_exists(path):
         try:
             db.files_get_metadata(path)
+            print("Folder exists already")
         except dropbox.exceptions.ApiError as e:
             if e.error.is_path() and e.error.get_path().is_not_found():
                 db.files_create_folder_v2(path)
                 print(f"Created folder: {path}")
             else:
+                print("Couldnt create the folder")
                 raise HTTPException(status_code=400, detail=str(e))
 
     # Ensure the folder structure exists first, otherwise quit
@@ -43,20 +45,26 @@ def upload_to_dropbox():
 
     with open(file_path, 'rb') as f:
         db.files_upload(f.read(), dropbox_destination_path, mode=dropbox.files.WriteMode("overwrite"))
-        
+    
+    print("Wrote the file into dropbox.\nFetching public video url now.")
+    
     public_link = get_public_link(db , dropbox_destination_path)
     
     return public_link
     
 def get_public_link(db , file_path):
     try:
-
-        shared_link = db.sharing_create_shared_link_with_settings(file_path)
-        return shared_link.url  # The public URL for your file
+        shared_links = db.sharing_list_shared_links(file_path , direct_only = True)
+        print(shared_links)
+        
+        if (shared_links.links):
+            return shared_links.links[0].url
+        else:
+            shared_link = db.sharing_create_shared_link_with_settings(file_path)
+            return shared_link.url  # The public URL for your file
     
     except dropbox.exceptions.ApiError as e:
         raise Exception(f"Error creating shared link: {e}")
         
 public_link = upload_to_dropbox()
-print("Uploaded successfully")
-print("The public link is - " , public_link)
+print("\nThe public link is - " , public_link)
